ad3ac164a71f042b1ada3f4385ff7f5ceb214db5
unknown <h.samir2011@gmail.com>
2021-12-27 16:36:42 +0200

    products test cases

--- capybara/spec/feautres/productsPage_spec.rb
+++ capybara/spec/feautres/productsPage_spec.rb
@@ -3,21 +3,20 @@ require 'spec_helper'
 feature 'Visit The Product Page' do
   background do
     visit '/index.php?id_category=8&controller=category'
+    all('.button.ajax_add_to_cart_button>span:nth-child(1)').first().click()
   end
+
   scenario 'verify that user can add product to cart' do
-    all('.button.ajax_add_to_cart_button>span:nth-child(1)').first().click()
     expect(page).to have_content('Product successfully added to your shopping cart')
   end
+
   scenario 'verify that user can add product and check order summary' do
-    all('.button.ajax_add_to_cart_button>span:nth-child(1)').first().click()
-    expect(page).to have_content('Product successfully added to your shopping cart')
     click_on 'Proceed to checkout'
     expect(page).to have_content 'SHOPPING-CART SUMMARY'
     expect(page).to have_content 'Printed Dress'
   end
+  
   scenario 'verify that user can add product and checkout it' do
-    all('.button.ajax_add_to_cart_button>span:nth-child(1)').first().click()
-    expect(page).to have_content('Product successfully added to your shopping cart')
     click_on 'Proceed to checkout'
     expect(page).to have_content 'SHOPPING-CART SUMMARY'
     expect(page).to have_content 'Printed Dress'
@@ -25,4 +24,10 @@ feature 'Visit The Product Page' do
     expect(page).to have_content 'AUTHENTICATION'
   end
 
+  scenario 'verify that user visits checkout from Cart icon' do
+    find(:css, '.continue.btn.btn-default').click()
+    find(:css, '.shopping_cart').click()
+    find(:css, '.shopping_cart>a').click()
+    expect(page).to have_content'SHOPPING-CART SUMMARY'
+  end
 end
