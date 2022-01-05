c7962b68c216c1551b18628735d7bf9944f4a2cc
unknown <h.samir2011@gmail.com>
2022-01-05 14:11:21 +0200

    submit message in contact us page

--- capybara/spec/feautres/contactUs_spec.rb
+++ capybara/spec/feautres/contactUs_spec.rb
@@ -14,7 +14,10 @@ feature 'Visit The-Internet Home Page' do
 
   scenario 'verify user can send message to contact us' do
     expect(page).to have_content('CUSTOMER SERVICE - CONTACT US')
+    fill_in 'message',:with => FFaker::BaconIpsum.sentence
+    find(:xpath,'//*[@id="email"]').set(FFaker::Internet.email)
+    find(:xpath,'//*[@id="id_contact"]/option[2]').click()
     click_on 'submitMessage'
-    expect(page).to have_content('There is 1 error')
+    expect(page).to have_content('Your message has been successfully sent to our team.')
   end
 end
